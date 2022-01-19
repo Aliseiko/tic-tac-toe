4d2c3a441596d4577da2c9803058a0397dffcdd4
Aliseiko <aliseiko.dev@gmail.com>
2022-01-19 14:05:05 +0300

    feat:add tic-tac-toe game logic, winner plate

--- tic-tac-toe/js/index.js
+++ tic-tac-toe/js/index.js
@@ -1,25 +1,74 @@
 const X = '<div class="x-figure"><span class="x-line-top"></span><span class="x-line-bottom"></span></div>',
-    O = '<div class="o-figure"></div>';
+    O = '<div class="o-figure"></div>',
+    movesX = [],
+    movesO = [],
+    winCombos = [[1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+        [1, 5, 9],
+        [7, 5, 3]
+    ];
+
+let figure = X,
+    currentFigure = 'X',
+    movesCount = 0,
+    winner;
 
 // ------------------- add eventlistner to gametable ----------------
 document.querySelector('.game-table').addEventListener('click', (event) => {
     if (event.target.classList.contains('game-table__game-cell') &&
-        !event.target.classList.contains('used')) {
-        alert('ok');
-        setX(event);
-        setUsedClass(event);
+        !event.target.classList.contains('used') &&
+        !winner) {
+        setFigure(figure);
+        setUsedClass();
+        addMove(currentFigure);
+        movesCount++;
+        if (movesCount > 4) checkWin(currentFigure);
+        currentFigure = (currentFigure === 'X') ? 'O' : 'X';
+        figure = (currentFigure === 'X') ? X : O;
+        showMovesCount();
+        showNextFigure();
+        console.log(winner)
     }
 
 })
 
-function setX(event) {
-    event.target.innerHTML = X;
+function setFigure(figure) {
+    event.target.innerHTML = figure;
 }
 
-function setO(event) {
-    event.target.innerHTML = O;
-}
-
-function setUsedClass(event) {
+function setUsedClass() {
     event.target.classList.add('used');
+}
+
+function showMovesCount() {
+    document.querySelector('.moves-count').textContent = movesCount;
+}
+
+function showNextFigure() {
+    document.querySelector('.next-move').textContent = currentFigure;
+}
+
+function addMove(fig) {
+    ((fig === 'X') ? movesX : movesO).push(+event.target.dataset.cellnumber);
+}
+
+function checkWin(fig) {
+    let m = ((fig === 'X') ? movesX : movesO);
+
+    for (let i = 0; i < 8; i++) {
+        if (m.includes(winCombos[i][0]) &&
+            m.includes(winCombos[i][1]) &&
+            m.includes(winCombos[i][2])) {
+            winner = fig;
+            return
+        }
+    }
+}
+
+function showWinner() {
+
 }
